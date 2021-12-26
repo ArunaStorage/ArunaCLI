9add040fb7913644f24be92280cf3f93aa87513b
Marius Dieckmann <mariusdieckmann90@gmail.com>
2021-12-26 16:59:48 +0100

    started implementing load function

--- src/load/load.rs
+++ src/load/load.rs
@@ -1,5 +1,7 @@
 use std::path::Path;
-
+use futures::future::{try_join_all};
+use scienceobjectsdb_rust_api::sciobjectsdbapi::models::v1::{Object, ObjectGroup};
+use tokio::io::AsyncWriteExt;
 use crate::{client::client, util::cli};
 
 pub struct Load {
@@ -16,10 +18,23 @@ impl Load {
         }
     }
 
-    async fn load_object_group(&self, basepath: &Path) {
-        
+    async fn load_object_groups(&self, basepath: &Path, object_groups: Vec<ObjectGroup>) {
+        let object_load_futures = Vec::new();
+
+        for object_group in object_groups {
+            for object in object_group.objects {
+            }
+        }
     }
 
-    async fn load_object_group_data(&self, basepath: &Path, links: Vec<String>) {
+    async fn load_object_data(&self, url: String, target_file_path: &Path) {
+        let mut file = tokio::fs::File::create(target_file_path).await.unwrap();
+        let mut url_response = reqwest::get(url).await.unwrap();
+
+        while let Some(chunk) = url_response.chunk().await.unwrap() {
+            file.write_all(&chunk).await.unwrap();
+        }
+
+        file.flush().await.unwrap();
     }
-}
+}   
