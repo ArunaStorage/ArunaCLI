d7bea558ee8c9b737494c5006e17f0c759efb948
lfbrehm <97600985+lfbrehm@users.noreply.github.com>
2022-06-14 10:28:57 +0200

    Wrote the CreateObjectGroupRequest more explicit for None option in object_ids.

--- src/create/create.rs
+++ src/create/create.rs
@@ -169,6 +169,7 @@ impl Create {
                 include_object_link: false,
                 labels: labels,
                 name: create_object_group_config.name,
+                update_objects: None,
                 ..Default::default()
             },
         };
